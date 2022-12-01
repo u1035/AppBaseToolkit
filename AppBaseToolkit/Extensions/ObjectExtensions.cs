06777519e7c49ce497fcb545cc5ec7db68d70e50
u1035 <u1035@mail.ru>
2022-12-01 19:02:31 +0300

    Added object extension method

---
+++ AppBaseToolkit/Extensions/ObjectExtensions.cs
@@ -0,0 +1,26 @@
+﻿using System.Diagnostics;
+using System.Runtime.CompilerServices;
+using JetBrains.Annotations;
+
+namespace AppBaseToolkit.Extensions;
+
+public static class ObjectExtensions
+{
+    /// <summary>
+    /// Returns provided value, checking that it's not null.
+    /// If the object is null - stops debugger.
+    /// </summary>
+    /// <param name="obj"></param>
+    /// <returns></returns>
+    [ContractAnnotation("null=>halt")]
+    [AssertionMethod]
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static object EnsureNotNull([AssertionCondition(AssertionConditionType.IS_NOT_NULL)] [System.Diagnostics.CodeAnalysis.NotNull]
+        this object? obj)
+    {
+#if DEBUG
+        Debug.Assert(obj != null, $"{obj} is null");
+#endif
+        return obj;
+    }
+}
